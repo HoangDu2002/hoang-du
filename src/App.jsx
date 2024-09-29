356ca4b2343d2413fdf137bf44adda09dd87b334
HoangDu2002 <147909975+HoangDu2002@users.noreply.github.com>
2024-09-29 10:29:37 +0700

    update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import Contact from './sections/Contact/Contact'
 import Footer from './sections/Footer/Footer'
-import Hero from './sections/hero/hero'
+import Hero from './sections/Hero/Hero'
 import Projects from './sections/Project/Projects'
 import Skills from './sections/Skills/Skills'
 
